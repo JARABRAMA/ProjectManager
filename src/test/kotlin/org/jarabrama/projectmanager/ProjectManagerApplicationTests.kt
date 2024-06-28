0bdf874e2bacd091a7ceda68e49eb9a53f9703b8
JARABRAMA <brayanjaraba2006@gmail.com>
2024-06-28 11:43:50 -0500

    initialization of the project

---
+++ src/test/kotlin/org/jarabrama/projectmanager/ProjectManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jarabrama.projectmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
